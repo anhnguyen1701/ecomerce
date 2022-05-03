5bcee22151f13d2aa7b368fd6cb974e7f82cf15e
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-03 21:32:37 +0700

    update & delete item cart

--- main/static/custom.js
+++ main/static/custom.js
@@ -80,6 +80,7 @@ $(document).ready(function (e) {
         var _vm = $(this);
         var _qty = $('#productQty').val();
         var _productId = $('.product-id').val();
+        var _productImage = $('.product-image').val();
         var _productTitle = $('.product-title').val();
         var _productPrice = $('.product-price').text();
         // Ajax
@@ -87,6 +88,7 @@ $(document).ready(function (e) {
             url: '/add-to-cart',
             data: {
                 id: _productId,
+                image: _productImage,
                 qty: _qty,
                 title: _productTitle,
                 price: _productPrice,
@@ -103,4 +105,52 @@ $(document).ready(function (e) {
         // End
     });
     // End
+
+    // Delete item from cart
+    $(document).on('click', '.delete-item', function () {
+        var _pId = $(this).attr('data-item');
+        var _vm = $(this);
+        // Ajax
+        $.ajax({
+            url: '/delete-from-cart',
+            data: {
+                id: _pId,
+            },
+            dataType: 'json',
+            beforeSend: function () {
+                _vm.attr('disabled', true);
+            },
+            success: function (res) {
+                $('.cart-list').text(res.totalitems);
+                _vm.attr('disabled', false);
+                $('#cartList').html(res.data);
+            },
+        });
+        // End
+    });
+
+    // Update item from cart
+    $(document).on('click', '.update-item', function () {
+        var _pId = $(this).attr('data-item');
+        var _pQty = $('.product-qty-' + _pId).val();
+        var _vm = $(this);
+        // Ajax
+        $.ajax({
+            url: '/update-cart',
+            data: {
+                id: _pId,
+                qty: _pQty,
+            },
+            dataType: 'json',
+            beforeSend: function () {
+                _vm.attr('disabled', true);
+            },
+            success: function (res) {
+                // $(".cart-list").text(res.totalitems);
+                _vm.attr('disabled', false);
+                $('#cartList').html(res.data);
+            },
+        });
+        // End
+    });
 });
