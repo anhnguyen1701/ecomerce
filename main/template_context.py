f8b15d7bbdc8de72bb705d284551815aafb4a68a
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-03 18:29:26 +0700

    min max filter

--- main/template_context.py
+++ main/template_context.py
@@ -1,14 +1,15 @@
-from .models import *
+from .models import Product,ProductAttribute
+from django.db.models import Min,Max
 
 def get_filters(request):
-    cats = Product.objects.distinct().values('category__title', 'category__id')
-    colors = ProductAttribute.objects.distinct().values('color__title', 'color__id', 'color__color_code')
-    sizes = ProductAttribute.objects.distinct().values('size__title', 'size__id')
-
-    data = {
-        'cats': cats,
-        'colors': colors,
-        'sizes': sizes
-    }
-
-    return data
+	cats=Product.objects.distinct().values('category__title','category__id')
+	colors=ProductAttribute.objects.distinct().values('color__title','color__id','color__color_code')
+	sizes=ProductAttribute.objects.distinct().values('size__title','size__id')
+	minMaxPrice=ProductAttribute.objects.aggregate(Min('price'),Max('price'))
+	data={
+		'cats':cats,
+		'colors':colors,
+		'sizes':sizes,
+		'minMaxPrice':minMaxPrice,
+	}
+	return data
